369bc86a1be21da67ebc0b2c7fc715b3c6949ef8
moyshik7 <moyshik7@gmail.com>
2021-04-18 14:46:11 +0600

    added settings.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client();
+
+const Settings = require("./settings");
---
+++ settings.js
@@ -0,0 +1,4 @@
+module.exports = {
+    PREFIX: "_",
+    BOT_TOKEN: ""
+}
